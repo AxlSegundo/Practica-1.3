881b11a3437e96f5e0ecbf771008ee0b1552ca16
Xim-Franco <161781284+Xim-Franco@users.noreply.github.com>
2024-03-17 15:12:14 -0600

    Punto 3: Definir la clase ProgramWindow

--- Sistema basado en ventanas.js
+++ Sistema basado en ventanas.js
@@ -8,3 +8,18 @@ Size.prototype.resize = function(newWidth, newHeight) {
   this.width = Math.max(1, newWidth); // Ancho mínimo de 1
   this.height = Math.max(1, newHeight); // Altura mínima de 1
 };
+
+
+// Punto 3: Definir la clase ProgramWindow
+function ProgramWindow() {
+  this.screenSize = new Size(800, 600);
+  this.size = new Size(); // Tamaño por defecto
+  this.position = new Position(); // Posición por defecto
+}
+
+// Crear una instancia de ProgramWindow
+let myWindow = new ProgramWindow();
+
+// Prueba para ver si se impriemn los valores por default. Se borrará después.
+console.log("Tamaño por defecto:", myWindow.size.width, myWindow.size.height); // 80 60
+console.log("Posición por defecto:", myWindow.position.x, myWindow.position.y); // 0 0
