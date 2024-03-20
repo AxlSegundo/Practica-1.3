fa5114ad65d12bd824770b57c0507ce0c735ccd1
tonantzin <tonaramirez2002@gmail.com>
2024-03-20 09:39:35 -0600

    tonantzin punto4

--- Sistema basado en ventanas.js
+++ Sistema basado en ventanas.js
@@ -34,3 +34,50 @@ class ProgramWindow {
   }
   
 }
+
+//punto4 practica javascrip
+
+class tamano{
+  constructor(ancho,alto){
+      this.ancho=ancho; //guarda el ancho del objeto.
+      this.alto=alto; //guarda el alto del objeto
+  }
+}
+
+class vetana_programa{
+  constructor(x,y){
+      this.x=x; //posicion horizontal de la ventana.
+      this.y=y; //posición vertical de la ventana.
+  }
+
+  redimensionar(tamano){
+      const anchominimo=1;
+      const anchomaximo=window.innerWidth - this.x;
+      const altomaximo=window.innerHeight - this.y;
+
+      let nuevoancho=tamano.ancho;
+      let nuevoalto=tamano.alto;
+
+      //limito el tamaño del nuevo ancho.
+      if (nuevoancho < anchominimo){
+          nuevoancho=anchominimo;
+          this.x += tamano.ancho - anchominimo;
+      }else if (nuevoancho > anchomaximo){
+          nuevoancho=anchomaximo;
+      }
+
+      if (nuevoancho < anchominimo){
+          nuevoalto=anchominimo;
+          this.y +=tamano.alto - anchominimo;
+      
+      }else if (nuevoalto > altomaximo){
+          nuevoalto = altomaximo;
+      }
+
+      console.log (`la ventana tiene un ancho de: ${nuevoancho} y un alto de: ${nuevoalto} ` );
+  }
+}
+
+const ventana1=new vetana_programa(400,300);
+//redimensiona la ventana 
+ventana1.redimensionoar(new tamano (400, 300));
