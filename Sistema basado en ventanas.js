bc9f0ab643580fdf3a2ddacfefe12d28271966a2
Axel-Segundo <axeljasegundo@gmail.com>
2024-03-20 13:17:23 -0600

    Cambios finales

--- Sistema basado en ventanas.js
+++ Sistema basado en ventanas.js
@@ -32,53 +32,16 @@ class ProgramWindow {
     this.size = new Size(); // Usa los valores por defecto de Size
     this.position = new Position(); // Usa los valores por defecto de Position
   }
-  
-}
-
-//punto4 practica javascrip
-
-class Size { //Punto uno
-  constructor(width = 80, height = 60) {
-    this.width = width;
-    this.height = height;
-  }
-
-  resize(newWidth, newHeight) {
-    this.width = newWidth;
-    this.height = newHeight;
-  }
-}
-
-// Definición de la clase Position
-class Position { //Punto dos
-  constructor(x = 0, y = 0) {
-    this.x = x;
-    this.y = y;
-  }
-
-  move(newX, newY) {
-    this.x = newX;
-    this.y = newY;
-  }
-}
-
-// Definición de la clase ProgramWindow
-class ProgramWindow { //Punto tres
-  constructor() {
-    this.screenSize = new Size(800, 600);
-    this.size = new Size(); // Usa los valores por defecto de Size
-    this.position = new Position(); // Usa los valores por defecto de Position
-  }
-
-  resize(newSize) { //punto cuatro
+// Punto 4: Creación de método para redimensionar
+  resize(newSize) {
     // Comprobar y ajustar la nueva anchura y altura dentro de los límites permitidos
     let maxWidth = this.screenSize.width - this.position.x;
     let maxHeight = this.screenSize.height - this.position.y;
     this.size.width = Math.max(1, Math.min(newSize.width, maxWidth));
     this.size.height = Math.max(1, Math.min(newSize.height, maxHeight));
   }
-
-  move(newPosition) { //punto cinco
+// Punto 5: Creación de método para mover la ventana
+  move(newPosition) { 
     // Ajustar la nueva posición dentro de los límites permitidos
     let maxX = this.screenSize.width - this.size.width;
     let maxY = this.screenSize.height - this.size.height;
@@ -87,7 +50,9 @@ class ProgramWindow { //Punto tres
   }
 }
 
-// Función para cambiar las propiedades de una ventana de programa
+
+
+// Punto 6: Función para cambiar las propiedades de una ventana de programa
 function changeWindow(window) { //punto seis
   // Cambiar el tamaño de la ventana
   window.resize(new Size(400, 300));
@@ -102,16 +67,20 @@ const size = new Size(1080,764);
 console.log(size.width, size.height);
 size.resize(1920,1080);
 console.log(size.width, size.height);
+console.log("");
 const point = new Position();
 console.log(point.x,point.y);
 point.move(100,200);
 console.log(point.x,point.y);
+console.log("");
 const programWindow = new ProgramWindow();
 console.log(programWindow.screenSize.width);
+console.log("");
 const newSize = new Size(600,400);
 programWindow.resize(newSize);
 console.log(programWindow.size.width);
 console.log(programWindow.size.height);
+console.log("");
 const newPosition = new Position(50,100);
 programWindow.move(newPosition);
 console.log(programWindow.position.x,programWindow.position.y);
